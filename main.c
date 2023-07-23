5f01cee25267be45473260a27bbbda112e489692
MoraisOlavo <olavoborges11@gmail.com>
2023-07-23 19:09:30 -0300

    É possível escolher qual o símbolo de cada jogador

--- main.c
+++ main.c
@@ -18,9 +18,24 @@ void main(){
 	pos_atual.vez=1;
 	pos_atual.valor=0;
 
+	char simbolo_primeiro_jogador;
+	char simbolo_segundo_jogador;
 	int aux;
+
+	printf("Digite qual será o símbolo do primeiro jogador\n");
+	scanf(" %c",&simbolo_primeiro_jogador);
+	printf("Char lido: %c\n",simbolo_primeiro_jogador);
+	fflush(stdin);
+
+	printf("Digite qual será o símbolo do segundo jogador\n");
+	scanf(" %c",&simbolo_segundo_jogador);
+	fflush(stdin);
+
+
 	while(VerificarVitoria(pos_atual)==0){       
 		if(pos_atual.vez==1){
+			printf("bolsonaro a\n");
+			PrintPosicao(pos_atual);
 			do{
 				scanf("%d",&aux);
 			}while(aux<0 || aux>8);
@@ -28,20 +43,18 @@ void main(){
 			aux=CalcularJogada(pos_atual);
 		}
 
-
 		if(pos_atual.tabuleiro[aux]!=' '){
 			printf("Posição já utilizada\n");
 			continue;
 		}
 
 		if(pos_atual.vez==1){
-			pos_atual.tabuleiro[aux]='O';
+			pos_atual.tabuleiro[aux]=simbolo_primeiro_jogador;
 		}else{
-			pos_atual.tabuleiro[aux]='X';
+			pos_atual.tabuleiro[aux]=simbolo_segundo_jogador;
 		}
 
 		pos_atual.vez*=-1;
-		PrintPosicao(pos_atual);
 	}
 
 	if(VerificarVitoria(pos_atual)==1){
